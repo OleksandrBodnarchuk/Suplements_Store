0f31526d8060792d208d98784717810824d3fa6d
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2020-08-30 11:01:13 +0200

    DataBaseConnection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let's start!");
+        final StandardServiceRegistry standardServiceRegistry =
+                new StandardServiceRegistryBuilder().configure().build();
+
+        try(SessionFactory sessionFactory = new MetadataSources(standardServiceRegistry)
+                .buildMetadata().buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+
+            Transaction transaction = session.beginTransaction();
+            transaction.commit();
+
+            session.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
