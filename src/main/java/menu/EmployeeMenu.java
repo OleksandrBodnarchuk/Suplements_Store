c2170cd63032c5333ddc45e276ad8354f36c6fb4
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2020-09-05 19:06:17 +0200

    Fixed Menu bugs

--- src/main/java/menu/EmployeeMenu.java
+++ src/main/java/menu/EmployeeMenu.java
@@ -1,6 +1,7 @@
 package menu;
 
 import model.Employee;
+import service.CustomerService;
 import service.ProductService;
 
 import java.io.BufferedReader;
@@ -15,31 +16,43 @@ public class EmployeeMenu {
 
     public void displayMenu() {
         System.out.println("Choose: " +
-                "\n 1. To view current product list" +
+                "\n 1. To view current product list." +
                 "\n 2. To add new product." +
-                "\n 3. To delete product using iD" +
-                "\n 4. To modify product using iD" +
-                "\n 5. To exit");
+                "\n 3. To add new client." +
+                "\n 4. To delete product using iD." +
+                "\n 5. To modify product using iD." +
+                "\n 6. To exit.");
         try {
             int choice = Integer.parseInt(reader.readLine());
-            switch (choice){
+            switch (choice) {
                 case 1:
                     productService.findAllProducts();
+                    break;
                 case 2:
                     productService.addProduct();
+                    break;
                 case 3:
-//                    productService.deleteProduct(); fix!!!!
+                    CustomerService customerService = new CustomerService();
+                    customerService.addCustomer();
+                    break;
                 case 4:
-//                    productService.updateProduct(); fix!!!!
+                    System.out.println("Please enter iD of the product you want to delete: \n");
+                    int iD = Integer.parseInt(reader.readLine());
+                    productService.deleteProduct(productService.findProduct(iD));
+                    break;
                 case 5:
+//                    productService.updateProduct(); fix!!!!
+                    break;
+                case 6:
                     System.out.println("Do you want to continue? (Y/N)");
-                    String quit=reader.readLine();
+                    String quit = reader.readLine();
                     quit = quit.toUpperCase();
-                    if (quit.equals("Y")){
-                       initialMenu.displayMenu();
-                    } else {
+                    if (quit.equals("Y")) {
                         displayMenu();
+                    } else {
+                        initialMenu.displayMenu();
                     }
+                    break;
             }
 
 
