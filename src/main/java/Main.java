dab2da2a15a1967b681d7b498451a41a2fad49a8
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2020-09-05 14:50:57 +0200

    Delete customer and product from DB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,12 @@
 
-import dao.CustomerDao;
-import model.Customer;
-import model.Products;
-import service.CustomerService;
+
 import service.ProductService;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ProductService productService = new ProductService();
-        productService.deleteProduct(productService.findProductById(81));
+        productService.addProduct();
     }
 }
